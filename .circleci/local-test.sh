927affb393187eae974fadf5ca72d8110d17e2bb
umihico <umihico@users.noreply.github.com>
2022-01-24 23:24:08 +0900

    fix: use public ecr and hide account id from circleci build log

--- .circleci/local-test.sh
+++ .circleci/local-test.sh
@@ -12,15 +12,15 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 REPO=circleci-stateless-ssm-parameter
 
-aws ecr describe-repositories --repository-names ${REPO} --region us-east-1 || \
-  aws ecr create-repository --repository-name ${REPO} --region us-east-1
+EXECUTOR_URI=$(aws ecr-public describe-repositories --repository-names ${REPO} --region us-east-1 --output text --query "repositories[0].repositoryUri" || \
+  aws ecr-public create-repository --repository-name ${REPO} --region us-east-1 --output text --query "repositories[0].repositoryUri")
 
-ECR=${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR}
-docker build .circleci -t ${ECR}/${REPO}:latest
-docker push ${ECR}/${REPO}:latest
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+docker build .circleci -t ${EXECUTOR_URI}:latest
+docker push ${EXECUTOR_URI}:latest
 
 circleci local execute --job simple \
+  -e EXECUTOR_URI=$EXECUTOR_URI \
   -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
